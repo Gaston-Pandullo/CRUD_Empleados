69d8928d43121083e9b1341dbfb077f8a6f29479
Gaston Pandullo <gpandullo3@gmail.com>
2025-05-07 20:19:08 -0300

    Agregar archivos de proyecto.

---
+++ CRUD_Empleados/Models/Empleado.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUD_Empleados.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Corre { get; set; }
+        public DateOnly FechaAdmision { get; set; }
+        public bool Activo { get; set; }
+    }
+}
