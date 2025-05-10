4845441fa192d9f850d6419ff8be68b6915e6572
Gaston Pandullo <gpandullo3@gmail.com>
2025-05-10 18:24:08 -0300

    Agregado de ventana para agregar datos y su logica. (Faltan botones)

---
+++ CRUD_Empleados/Controllers/EmpleadoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRUD_Empleados.Data;
+using CRUD_Empleados.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Empleados.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+        public EmpleadoController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Lista()
+        {
+            List<Empleado> listaEmplados = await _dbContext.Empleados.ToListAsync();
+            return View(listaEmplados);
+        }
+        [HttpGet]
+        public IActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Nuevo(Empleado empleado)
+        {
+            await _dbContext.Empleados.AddAsync(empleado);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
+        }
+    }
+}
